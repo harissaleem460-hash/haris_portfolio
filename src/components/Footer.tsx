29aa3cfb07594b7b6f0534081348eafc6eba12d3
harissaleem460-hash <harissaleem460@gmail.com>
2026-03-04 11:39:31 +0500

    Remove unused GitHubIcon import from Footer and Main components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import '../assets/styles/Footer.scss'
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import profileImage from '../assets/images/Portfolio.jpeg';
